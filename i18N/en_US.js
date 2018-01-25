ca3671b576a04429221ff80167129780d1c4f541
Al Dynak <adynak@gmail.com>
2018-01-25 12:52:36 -0800

    first commit

---
+++ i18N/en_US.js
@@ -0,0 +1,12 @@
+txtPrompts = {
+	brandName: 'Filters',
+	titleCase: 'Title Case',
+	text: 'Text',
+	dashFI: 'Account',
+	formNumber: 'Form Number',
+	readyToTestMessage: 'Ready To Test'	
+};
+
+txtMessages = {
+	readyToTest: "This message is for CUSTOMER. " + "\n" + "I have received your request and have added the form you requested on your DASHFI account for form # FORM_NUMBER." + "\n" + "Please test this form on a fully-loaded deal, check for line-up and content. I have emailed you a fax cover sheet.  Please print out the fax cover sheet and either use CDK Service Connect or fax an example of the form with a fully-loaded deal printed, note any corrections. Please keep in mind you have until 02/25/2018 for any changes to be completed at no additional charge.If we do not hear from you in 2 business days, we will close this case." + "\n" + "Thank you," + "\n" + "Al Dynak" + "\n" + "Forms Fulfillment Center"
+};
